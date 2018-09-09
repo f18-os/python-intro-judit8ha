5b7eb8a063c0161856e4614e0a2cf21cfde1113a
judit8ha <xjoliveros@miners.utep.edu>
2018-09-09 01:18:54 -0600

    myShell initial commit - not working

--- myShellDir/myShell.py
+++ myShellDir/myShell.py
@@ -2,7 +2,47 @@
 
 import os, sys, time, re
 
-pid = os.getpid()               # get and remember pid
+# xjo
+wT = '>'
+rF = '<'
+pipe = '|'
+args = {}  # making an directory for args
+
+args = input('myShell-' + os.getcwd() + ': ').split(' ')  # splitting input into a directory of arguments
+
+
+# METHOD FOR INSTRUCTION EXECUTION
+def execute(a):
+    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+        program = "%s/%s" % (dir, a[0])
+        try:
+            os.execve(program, a, os.environ)  # try to exec program
+        except FileNotFoundError:  # ...expected
+            pass  # ...fail quietly
+
+    os.write(2, ("Child:    Error: Could not exec %s\n" % a[0]).encode())
+    sys.exit(1)  # terminate with error
+
+def write_to(fileName):
+    os.close(1)  # redirect child's stdout
+    sys.stdout = open(fileName, "w")
+    fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+    os.set_inheritable(fd, True)
+
+
+    rc = os.fork()
+
+
+def display(filename):
+
+
+
+
+if args.length() != 0 & args.length() < 6:
+    if args.length() == 1:
+        execute(args)
+
+pid = os.getpid()  # get and remember pid
 
 os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 
@@ -12,30 +52,30 @@ if rc < 0:
     os.write(2, ("fork failed, returning %d\n" % rc).encode())
     sys.exit(1)
 
-elif rc == 0:                   # child
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+elif rc == 0:  # child
+    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
                  (os.getpid(), pid)).encode())
-    args = ["wc", "p3-exec.py"]
+    # args = ["wc", "p3-exec.py"]
 
-    os.close(1)                 # redirect child's stdout
+    os.close(1)  # redirect child's stdout
     sys.stdout = open("p4-output.txt", "w")
-    fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+    fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
     os.set_inheritable(fd, True)
     os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
 
-    for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, args[0])
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly 
+    #   for dir in re.split(":", os.environ['PATH']): # try each directory in path
+    #       program = "%s/%s" % (dir, args[0])
+    #       try:
+    #           os.execve(program, args, os.environ) # try to exec program
+    #       except FileNotFoundError:             # ...expected
+    #           pass                              # ...fail quietly
 
-    os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-    sys.exit(1)                 # terminate with error
+    # os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+    # sys.exit(1)  # terminate with error
 
-else:                           # parent (forked ok)
-    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
+else:  # parent (forked ok)
+    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
                  (pid, rc)).encode())
     childPidCode = os.wait()
-    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
+    os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
                  childPidCode).encode())
---
+++ wordCount.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+import string
+# print ui for file names
+# open file to read from
+fileNameIn = input('Please enter filename ')
+ff = open(fileNameIn, "r")
+
+# open file for writing or create if it does not exists
+fileNameOut = input('Enter output file name ')
+f = open(fileNameOut, "w")
+
+
+# create a dictionary
+words = {}
+
+#strip punctuation
+for word in ff.read().split():
+    cleanWord = ""
+    for c in word:
+        if c not in string.punctuation:
+            cleanWord += c
+#add word or increase count into dictionary
+    if cleanWord.lower() not in words:
+        words[cleanWord.lower()] = 1
+    else:
+        words[cleanWord.lower()] += 1
+
+for m, n in words.items():
+    f.write(str(m) + ' : ' + str(n) + '\n\n')
+
+
+f.close()
+
+print("process finished")
