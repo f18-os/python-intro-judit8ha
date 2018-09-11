894ce4c73bf8df63495f1ea23eb05d1f46ce5617
judit8ha <xjoliveros@miners.utep.edu>
2018-09-10 19:48:47 -0600

    working project all requirements met. Needs cleanup

--- myShellDir/myShell.py
+++ myShellDir/myShell.py
@@ -8,8 +8,10 @@ rF = '<'
 
 args = []  # making a list of args
 
+
 # splitting input into a directory of arguments
 args = input('myShell-' + os.getcwd() + ': ').split(' ')
+cmd = [args[0]]
 
 
 # METHOD FOR INSTRUCTION EXECUTION
@@ -25,29 +27,31 @@ def execute(cmds):
     sys.exit(1)  # terminate with error
 
 
-def redirect_fd1(fileName):  #write to a file not to screen
+def redirect_fd1(fileName):  # write to a file not to screen
     os.close(1)  # redirect child's stdout
     sys.stdout = open(fileName, "w")
     fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
     os.set_inheritable(fd, True)
     os.write(2, ("file directory  fd=%d redirected for  writing\n" % fd).encode())
+     # sys.exit(0)
 
 
-def redirect_fd0(filename):  #read from file not keyboard
-    os.close(0) #redirect child's stdin
+def redirect_fd0(filename):  # read from file not keyboard
+    os.close(0)  # redirect child's stdin
     sys.stdin = open(filename, "r")
-    fd = sys.stdout.fileno()
+    fd = sys.stdin.fileno()
     os.set_inheritable(fd, True)
-    os.write(2, ("file directory fd = %d redirected for reading\n" % fd))
+    os.write(2, ("file directory fd = %d redirected for reading\n" % fd).encode())
+    # sys.exit(0)
 
 
 # REGULAR EXPRESSION SYNTAX VARIATIONS
-
     # 1 ARGUMENT
-if len(args) is 1:
+if len(args) is 1:    #working
     execute(args)
 # 2 ARGUMENTS
-elif len(args[0]) is 2:
+elif len(args) is 2: #working
+    pid = os.getpid()
     rc = os.fork()
 
     if rc < 0:
@@ -56,12 +60,15 @@ elif len(args[0]) is 2:
 
     elif rc == 0:  # child
         redirect_fd0(args[1])
-        execute(args)
+        execute(cmd)
+        sys.exit(0)
 
     else:
         os.write(1, ("forked = %d \n" % rc).encode())
+        cPid: os.wait()
 # 3 ARGUMENTS
 elif len(args) is 3:
+    pid = os.getpid()
     rc = os.fork()
 
     if rc < 0:
@@ -70,22 +77,27 @@ elif len(args) is 3:
 
     elif rc == 0:
         # rc = os.fork()
+        val: str
+        for val in args: print("%s \n" % val)   ## DEBUGGING CODE
         if args[1] is '>':
             redirect_fd1(args[2])
-            execute(args)
+            execute(cmd)
+            sys.exit(0)
 
         elif args[1] is '<':
             redirect_fd0(args[2])
-            execute(args)
+            execute(args[0])
+            sys.exit(0)
 
         else:
             os.write(1, ("command not recognized, exiting \n" % rc).encode())
             sys.exit(1)
     else:
-
         os.write(1, ("forked = %d \n" % rc).encode())
+        cPid: os.wait()
 # 4 ARGUMENTS
 elif len(args) is 4:
+    pid = os.getpid()
     rc = os.fork()
     if rc < 0:
         os.write(2, ("fork failed, returning %d\n" % rc).encode())
@@ -95,16 +107,22 @@ elif len(args) is 4:
         if args[2] is '>':
             redirect_fd1(args[3])
             redirect_fd0(args[1])
-            execute(args)
+            execute(cmd)
+            sys.exit(0)
 
         elif args[2] is '<':
             redirect_fd0(args[1])
             redirect_fd1(args[3])
-            execute(args)
+            execute(cmd)
+            sys.exit(0)
 
         else:
             os.write(1, ("command not recognized, exiting \n" % rc).encode())
             sys.exit(1)
+    else:
+        cPid: os.wait()
+else:
+    os.write(2, ("Error: number of arguments is not accepted \n").encode())
 
 
 
