f7acb773fb1445aaf881cdbe670bacdf1f46444b
judit8ha <xjoliveros@miners.utep.edu>
2018-09-10 19:51:23 -0600

    working project all requirements met. final commit for HW2

--- myShellDir/myShell.py
+++ myShellDir/myShell.py
@@ -2,16 +2,13 @@
 
 import os, sys, time, re
 
-# xjo
-wT = '>'
-rF = '<'
 
 args = []  # making a list of args
 
 
 # splitting input into a directory of arguments
 args = input('myShell-' + os.getcwd() + ': ').split(' ')
-cmd = [args[0]]
+cmd = [args[0]] # creates a list of one argument leaves opportunity for addtl commands later
 
 
 # METHOD FOR INSTRUCTION EXECUTION
@@ -46,7 +43,7 @@ def redirect_fd0(filename):  # read from file not keyboard
 
 
 # REGULAR EXPRESSION SYNTAX VARIATIONS
-    # 1 ARGUMENT
+# 1 ARGUMENT
 if len(args) is 1:    #working
     execute(args)
 # 2 ARGUMENTS
@@ -125,45 +122,3 @@ else:
     os.write(2, ("Error: number of arguments is not accepted \n").encode())
 
 
-
-
-
-
-#
-# pid = os.getpid()  # get and remember pid
-#
-# os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
-#
-# rc = os.fork()
-#
-# if rc < 0:
-#     os.write(2, ("fork failed, returning %d\n" % rc).encode())
-#     sys.exit(1)
-#
-# elif rc == 0:  # child
-#     os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
-#                  (os.getpid(), pid)).encode())
-#     # args = ["wc", "p3-exec.py"]
-#
-#     os.close(1)  # redirect child's stdout
-#     sys.stdout = open("p4-output.txt", "w")
-#     fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
-#     os.set_inheritable(fd, True)
-#     os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
-#
-#     #   for dir in re.split(":", os.environ['PATH']): # try each directory in path
-#     #       program = "%s/%s" % (dir, args[0])
-#     #       try:
-#     #           os.execve(program, args, os.environ) # try to exec program
-#     #       except FileNotFoundError:             # ...expected
-#     #           pass                              # ...fail quietly
-#
-#     # os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-#     # sys.exit(1)  # terminate with error
-#
-# else:  # parent (forked ok)
-#     os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
-#                  (pid, rc)).encode())
-#     childPidCode = os.wait()
-#     os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
-#                  childPidCode).encode())
